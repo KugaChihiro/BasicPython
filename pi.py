d921f389c97cd881ce166df7f23d12281ae891ca
KugaChihiro <c.kuga.24.4.1410@4nonome.com>
2024-05-02 11:47:50 +0900

    python入門課題提出１回目

--- pi.py
+++ pi.py
@@ -2,6 +2,7 @@ text = """
     How I want a drink, alcoholic of course, after the heavy chapters involving
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
-
-# TODO
-
+edited_text = text.replace(",", "").replace(".", "")
+wordslist = edited_text.split( )
+numberslist = list(map(len, wordslist))
+print("".join(map(str,numberslist)))
